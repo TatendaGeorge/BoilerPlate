fd14e763ba5cba73a6682a785dd6ef3d0d9380cf
TeeGeorge <georgetatenda27@gmail.com>
2021-03-09 10:51:33 +0200

    First commit

---
+++ aspnet-core/src/FirstBoilerPlateApp.EntityFrameworkCore/EntityFrameworkCore/FirstBoilerPlateAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using FirstBoilerPlateApp.Authorization.Roles;
+using FirstBoilerPlateApp.Authorization.Users;
+using FirstBoilerPlateApp.MultiTenancy;
+
+namespace FirstBoilerPlateApp.EntityFrameworkCore
+{
+    public class FirstBoilerPlateAppDbContext : AbpZeroDbContext<Tenant, Role, User, FirstBoilerPlateAppDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public FirstBoilerPlateAppDbContext(DbContextOptions<FirstBoilerPlateAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
